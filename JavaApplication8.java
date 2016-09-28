0ffa165be44fe281a6ecaeb6d3a59f7873e8d68c
Kyle Turner <kylet2014@outlook.com>
2016-09-28 11:43:27 +0100

    Add files via upload

---
+++ JavaApplication8.java
@@ -0,0 +1,27 @@
+
+package javaapplication8;
+
+
+public class JavaApplication8 {
+
+  
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num1 = myScanner.nextInt();
+        System.out.println("Enter a second number");
+        int num2 = myScanner.nextInt();
+        System.out.println("Would you like to add, substract, multiply or divide the numbers");
+        String option = myScanner.next();
+                
+        if (option.contains("add")) {
+            System.out.println("Your answer is " + (num1+num2));
+        }else if (option.contains("subtract")) {
+            System.out.println("Your answer is " + (num1-num2));
+        }else if (option.contains("divide")) {
+            System.out.println("Your answer is " + (num1/num2));
+        }else if (option.contains("multiply")) {
+            System.out.println("Your answer is " + (num1*num2));    
+    }
+    
+}
